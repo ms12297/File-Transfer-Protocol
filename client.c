e90ceeca4daed2188fd7ee69a5c80249c899f8c0
ms12297 <ms12297@nyu.edu>
2023-03-05 06:51:05 +0400

    init: boilerplate client

---
+++ client.c
@@ -0,0 +1,111 @@
+#include <stdio.h> // header for input and output from console : printf, perror
+#include<string.h> // strcmp
+#include<sys/socket.h> // for socket related functions
+#include<arpa/inet.h> // htons
+#include <netinet/in.h> // structures for addresses
+#include<unistd.h> // header for unix specic functions declarations : fork(), getpid(), getppid()
+#include<stdlib.h> // header for general fcuntions declarations: exit()
+#include <sys/stat.h> // for file size using stat()
+
+#define PORT 9000 // port number for FTP, NOT 21?!
+
+unsigned long ftp_port;
+char ipAddress[256];
+
+
+
+int main()
+{
+	//socket
+	int socket_FTP = socket(AF_INET,SOCK_STREAM,0);
+	if(socket_FTP < 0)
+	{
+		perror("socket:");
+		exit(-1);
+	}
+    int login = 0;
+
+
+	//setsock
+	int value  = 1;
+    //setsockopt to reuse ports and prevent bind errors
+	setsockopt(socket_FTP,SOL_SOCKET,SO_REUSEADDR,&value,sizeof(value)); //&(int){1},sizeof(int)
+
+
+    //server address
+	struct sockaddr_in server_addr;
+	bzero(&server_addr,sizeof(server_addr));
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(PORT);
+	server_addr.sin_addr.s_addr = INADDR_ANY; //INADDR_ANY, INADDR_LOOP, inet_addr("127.0.0.1")
+
+
+	//connect
+    if(connect(socket_FTP,(struct sockaddr*)&server_addr,sizeof(server_addr))<0)
+    {
+        perror("connect");
+        exit(-1);
+    }
+    
+
+    // client address - finding port that is free to use for data channel
+    struct sockaddr_in client_addr;
+    socklen_t client_size = sizeof(client_addr);
+    if (getsockname(socket_FTP, (struct sockaddr *)&client_addr, &client_size) < 0) {
+        perror("getsockname");
+        exit(-1);
+    }
+    ftp_port = htons(client_addr.sin_port);
+    sprintf(ipAddress, "%s", inet_ntoa(client_addr.sin_addr));
+
+
+    //welcome
+	printf("220 Service ready for new user.\n");
+    char *command; //command input var pointer
+
+    //accept 1kb chunks of data
+	char buffer[1024];
+    char response[1024];
+    char client_name[1024] = " ";
+    size_t buffer_size = 256;
+    command = (char *)malloc(buffer_size * sizeof(char));
+
+
+	while(1) // client connection loop - ONLY LOCAL CALLS FOR NOW
+	{
+        // reseting the buffers and command
+	    bzero(&command, sizeof(command));
+        bzero(&buffer, sizeof(buffer));
+        bzero(&response, sizeof(response));
+
+        printf("ftp> ");
+
+        getline(&command, &buffer_size, stdin);
+        if (strcmp(command, "\n") == 0)
+		    continue;
+
+        // spliting command into tokens
+        char *cmd1 = strtok(command, " \n"); // first token
+        char *cmd2 = strtok(NULL, " \n");    // second token
+
+        
+
+        // boilerplate
+
+		if(send(socket_FTP,cmd1,strlen(cmd1),0) < 0)
+		{
+			perror("send");
+			exit(-1);
+		}
+		bzero(cmd1,sizeof(cmd1));
+
+		int bytes = recv(socket_FTP,cmd1,sizeof(cmd1),0);
+		printf("Server response: %s\n",cmd1);
+
+        // boilerplate
+	}
+
+	close(socket_FTP);
+	return 0;
+}
+
